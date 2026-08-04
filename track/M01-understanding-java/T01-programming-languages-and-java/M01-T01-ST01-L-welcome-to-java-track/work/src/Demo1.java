30a20fe95c72cc03919098db161a8b2cc118661e
yaswanth <yaswanthadhonii@gmail.com>
2026-08-04 12:16:52 +0530

    day1

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Demo1.java
@@ -0,0 +1,12 @@
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("monday");
+        System.out.println("Tuesday wednesday");
+        System.out.println("Thursday");
+        System.out.println("Fridaysaturday");
+        System.out.println("Sunday");
+    }
+}

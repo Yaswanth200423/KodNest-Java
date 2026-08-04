f82eb3cbf1e941e8c4adce8acfc8395d46934cc9
yaswanth <yaswanthadhonii@gmail.com>
2026-08-04 12:45:32 +0530

    day2

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Demo2.java
@@ -0,0 +1,17 @@
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+        int num=2;
+        if (num > 0) {
+            System.out.println("number is positive");
+        }else if(num<0)
+        {
+            System.out.println("number is negative");
+        }
+         else {
+            System.out.println("zero");
+        }
+
+    }
+}

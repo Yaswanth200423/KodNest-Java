f283a748eb5b49847a9a428754d36ffe6458f03d
yaswanth <yaswanthadhonii@gmail.com>
2026-08-06 11:46:20 +0530

    nestedwhileloop

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Formain.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Formain.java
@@ -1,8 +1,8 @@
 public class Formain{
     public static void main(String[] args){
         for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
-            System.out.println(j);
+        for(int =1;i<=5;i++){
+            System.out.println(i);
         }
         System.out.println();
     }

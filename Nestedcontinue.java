f5a7c66436f15a780d55c789da882978118817b1
yaswanth <yaswanthadhonii@gmail.com>
2026-08-06 13:08:19 +0530

    break outer

---
+++ Nestedcontinue.java
@@ -0,0 +1,12 @@
+public class Nestedcontinue{
+    public static void main(String[] args){
+        outer: for(int i=1;i<=3;i++){
+            inner: for(int j=1;j<=2;j++){
+                   if( j== 2){
+                   break outer;
+                   }
+                 System.out.println("i :"+i + "j:"+j);
+             }
+    }
+}
+}

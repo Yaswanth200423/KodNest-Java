9ae8210458d0bfee3257794b477597202f24260d
yaswanth <yaswanthadhonii@gmail.com>
2026-08-06 12:53:39 +0530

    nested break

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Break.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Break.java
@@ -1,11 +1,10 @@
-public class Continue{
+public class Break{
     public static void main(String[] args){
         for(int i=1;i<=10;i++){
-            if(i == 6){
-                continue;
-
-            }
             System.out.println(i);
+        if(i == 6){
+            break;
+        }
     }
-  }
+    }
 }
